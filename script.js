24cb52cd044d7a97390da93fd4bc83247110b64e
Adarsha Bairagi <124226099+adarsha5@users.noreply.github.com>
2025-05-11 19:17:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ // syntax ...................
+ x='Central Calcutta Polytechnic' // A serise of characters is called string. A string in javascript can nt be changed
+ console.log(x);
+
+ var y="6";
+ console.log(y); // y is used like a pointer
+ var abc=6;
+ var amount2=10;
+
+ console.log(abc);
+
